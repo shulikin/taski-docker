413bb5c923b639773a410eb4de8d341e122a2abd
shulikin <alex@shulikin.com>
2024-09-19 13:11:38 +0400

    190924

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '89.169.173.112', 'http://localhost/', 'https://fume.webhop.me/']
+ALLOWED_HOSTS = ['89.169.173.112', 'https://fume.webhop.me/', 'localhost', '127.0.0.1']
 
 # Application definition
 
